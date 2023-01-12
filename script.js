e43d8d69dfe022a88343cd3969c5190440ccfcc7
sarahangle <sna44@cornell.edu>
2023-01-11 23:13:13 -0800

    Implement functionality messily

--- script.js
+++ script.js
@@ -1,22 +1,7 @@
+/* eslint-disable radix */
 /* eslint-disable no-use-before-define */
 // controls actions unique to each player
-const playerFactory = (playerID) => {
-  const playPosition = (row, col) => {
-    if (gameBoard.playPosition(playerID, row, col)) {
-      if (gameBoard.checkWin(row, col)) {
-        win();
-      }
-      return true;
-    }
-    return false;
-  };
-  const win = () => {
-    console.log(`Player ${playerID} wins!!`);
-  };
-  return {
-    playerID, playPosition, win,
-  };
-};
+const playerFactory = (playerID) => ({ playerID });
 
 // controls array of x's and o's
 const gameBoard = (() => {
@@ -41,11 +26,18 @@ const gameBoard = (() => {
     ) { return true; }
     return false;
   };
+  const checkTie = () => {
+    if (!board[0].includes('') && !board[1].includes('') && !board[2].includes('')) {
+      return true;
+    }
+    return false;
+  };
   return {
     board,
     newGame,
     writePosition,
     checkWin,
+    checkTie,
   };
 })();
 
@@ -55,16 +47,112 @@ const gameController = (() => {
   const playerO = playerFactory('O');
   let activePlayer = playerX;
 
+  const newGame = () => {
+    gameBoard.newGame();
+    activePlayer = playerX;
+  };
   const switchPlayer = () => {
     if (activePlayer === playerX) { activePlayer = playerO; } else { activePlayer = playerX; }
+    gameDisplay.updateDirection(activePlayer.playerID, false);
   };
+  const selectSquare = (row, col) => {
+    gameDisplay.updateSquare(row, col, activePlayer.playerID);
+    gameBoard.writePosition(activePlayer.playerID, row, col);
+    if (gameBoard.checkWin(row, col)) {
+      win();
+    } else if (gameBoard.checkTie()) {
+      tie();
+    } else { switchPlayer(); }
+  };
+  function win() {
+    gameDisplay.win(activePlayer.playerID);
+  }
+  function tie() {
+    gameDisplay.tie(activePlayer.playerID);
+  }
 
   return {
+    newGame,
     switchPlayer,
+    selectSquare,
   };
 })();
 
 // uses game logic to effect display of game
 const gameDisplay = (() => {
+  const gameSetup = () => {
+    const allGridSquares = document.querySelectorAll('.board-square');
+    allGridSquares.forEach((square) => {
+      square.classList.remove('ex');
+      square.classList.remove('oh');
+      square.classList.add('empty');
+      square.addEventListener('click', squareClick);
+    });
+  };
+  function squareClick(event) {
+    const row = parseInt(event.target.id[0]);
+    const col = parseInt(event.target.id[2]);
+    gameController.selectSquare(row, col);
+    event.target.removeEventListener('click', squareClick);
+  }
+  const updateSquare = (row, col, player) => {
+    const gridSquare = document.getElementById(`${row}-${col}`);
+    gridSquare.classList.remove('empty');
+    if (player === 'X') {
+      gridSquare.classList.add('ex');
+    } else if (player === 'O') {
+      gridSquare.classList.add('oh');
+    }
+  };
+  const updateDirection = (playerID, type) => {
+    const directionBlurb = document.querySelector('.information');
+    let player = '1';
+    if (playerID === 'O') { player = '2'; }
 
+    if (type === 'win') {
+      directionBlurb.textContent = `Player ${player} wins!`;
+    } else if (type === 'tie') {
+      directionBlurb.textContent = 'It\'s a tie!';
+    } else {
+      directionBlurb.textContent = `Player ${player}'s turn!`;
+    }
+  };
+  function activateButton() {
+    const buttonElement = document.querySelector('button');
+    buttonElement.classList.add('active');
+    buttonElement.addEventListener('click', buttonNewGame);
+  }
+  function buttonNewGame(event) {
+    gameController.newGame();
+    gameDisplay.gameSetup();
+    event.target.removeEventListener('click', buttonNewGame);
+    event.target.classList.remove('active');
+    updateDirection('X', false);
+  }
+  const win = (playerID) => {
+    // Remove event listeners so no one can pick more squaraes
+    const allGridSquares = document.querySelectorAll('.board-square');
+    allGridSquares.forEach((square) => {
+      square.removeEventListener('click', squareClick);
+    });
+    // Update blurb to say who won
+    updateDirection(playerID, 'win');
+    // Show button to start new game
+    activateButton();
+  };
+  const tie = (playerID) => {
+    // Update blurb to say who won
+    updateDirection(playerID, 'tie');
+    // Show button to start new game
+    activateButton();
+  };
+  return {
+    gameSetup,
+    updateSquare,
+    updateDirection,
+    win,
+    tie,
+  };
 })();
+
+gameDisplay.gameSetup();
